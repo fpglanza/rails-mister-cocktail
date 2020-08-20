9b07e246f72640f7542bf30259508cc4f68c8309
Filippo Lanza <fpg.lanza@gmail.com>
2020-08-20 17:45:13 +0200

    Added functioning master-cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "ginger")
+Ingredient.create(name: "teriyaki")
+
+Cocktail.create(name: "whiskey sour")
+Cocktail.create(name: "gin & tonic")
+Cocktail.create(name: "long island ice tea")
+Cocktail.create(name: "white russian")
+Cocktail.create(name: "basil smash")
+
+Dose.create(description: "6 leaves", cocktail_id: 1, ingredient_id: 1)
+Dose.create(description: "1.5 oz", cocktail_id: 2, ingredient_id: 2)
+# Dose.create(description: "4.5 cl")
+# Dose.create(description: "9 cl")
+# Dose.create(description: "2 cl")
